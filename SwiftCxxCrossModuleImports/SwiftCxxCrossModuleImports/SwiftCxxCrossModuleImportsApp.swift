fb32db19559aa05955995f96fac7101d751b5d46
Marc Rousavy <me@mrousavy.com>
2026-01-19 18:35:41 +0100

    Create Xcode proj

---
+++ SwiftCxxCrossModuleImports/SwiftCxxCrossModuleImports/SwiftCxxCrossModuleImportsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCxxCrossModuleImportsApp.swift
+//  SwiftCxxCrossModuleImports
+//
+//  Created by Marc Rousavy on 19.01.26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCxxCrossModuleImportsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
